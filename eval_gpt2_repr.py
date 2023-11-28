161d38c9c512ace36727b239ce37c06b2973ba1a
Joseph Ugarte <jeugarte14@gmail.com>
2023-11-28 13:50:31 -0500

    Ran experiment 1 on more stimuli

--- eval_gpt2_repr.py
+++ eval_gpt2_repr.py
@@ -7,6 +7,10 @@ from sklearn.svm import SVC
 from sklearn.metrics import classification_report
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
+# Preprocess input data better (i.e. remove <eos> tags)
+# Replicate to past HW2
+# Run past HW2
+# COnsult GPT about making their own classifier using Linear Regression
 
 def preprocess():
     with open("./experiments/wh_adjunct/input.txt", 'r') as sentence_file:
@@ -31,7 +35,7 @@ def train_test_sets(hiddens):
     when = [("wh-adj", v) for v in when]
     where = hiddens[100:120]
     random.shuffle(where)
-    where = [("wh-adj", v) for v in where]
+    where = [("comp", v) for v in where]
 
     train = that[:15] + why[:15] + how[:15] + when[:15] + where[:15] + whether[:15]
     random.shuffle(train)
@@ -44,28 +48,32 @@ def train_test_sets(hiddens):
     
 def logistic_regression(train, test):
     # Prepare the training data
-    X_train = torch.stack([h for _, h in train])
+    # X_train = torch.stack([h for _, h in train])
+    # y_train = [label for label, _ in train]
+    X_train = np.array([h for _, h in train])
     y_train = [label for label, _ in train]
 
     # Prepare the testing data
-    X_test = torch.stack([h for _, h in test])
+    # X_test = torch.stack([h for _, h in test])
+    # y_test = [label for label, _ in test]
+    X_test = np.array([h for _, h in test])
     y_test = [label for label, _ in test]
 
     # Convert to NumPy arrays if using scikit-learn
-    X_train_np = X_train.detach().numpy()
-    y_train_np = np.array(y_train)
-    X_test_np = X_test.detach().numpy()
-    y_test_np = np.array(y_test)
+    # X_train_np = X_train.detach().numpy()
+    # y_train_np = np.array(y_train)
+    # X_test_np = X_test.detach().numpy()
+    # y_test_np = np.array(y_test)
 
     # Initialize and train the logistic regression classifier
     clf = LogisticRegression(max_iter=1000)
-    clf.fit(X_train_np, y_train_np)
+    clf.fit(X_train, y_train)
 
     # Predict on the test set and evaluate
-    y_pred = clf.predict(X_test_np)
+    y_pred = clf.predict(X_test)
     print("Logistic Regression Classifier")
-    print(classification_report(y_test_np, y_pred))
-    return y_pred, y_test_np
+    print(classification_report(y_test, y_pred))
+    return y_pred, y_test
 
 def incr_logistic_regression(that, whether, why, how, when, where):
 
@@ -120,25 +128,29 @@ def incr_rlogistic_regression(whole):
 
 def svm(train, test):
     # Prepare the training data
-    X_train = torch.stack([h for _, h in train])
+    # X_train = torch.stack([h for _, h in train])
+    # y_train = [label for label, _ in train]
+    X_train = np.array([h for _, h in train])
     y_train = [label for label, _ in train]
 
     # Prepare the testing data
-    X_test = torch.stack([h for _, h in test])
+    # X_test = torch.stack([h for _, h in test])
+    # y_test = [label for label, _ in test]
+    X_test = np.array([h for _, h in test])
     y_test = [label for label, _ in test]
 
     # Convert to NumPy arrays if using scikit-learn
-    X_train_np = X_train.detach().numpy()
-    y_train_np = np.array(y_train)
-    X_test_np = X_test.detach().numpy()
-    y_test_np = np.array(y_test)
+    # X_train_np = X_train.detach().numpy()
+    # y_train_np = np.array(y_train)
+    # X_test_np = X_test.detach().numpy()
+    # y_test_np = np.array(y_test)
 
     # Using a linear kernel first
     svm_clf = SVC(kernel='linear')
-    svm_clf.fit(X_train_np, y_train_np)
-    y_pred_svm = svm_clf.predict(X_test_np)
+    svm_clf.fit(X_train, y_train)
+    y_pred_svm = svm_clf.predict(X_test)
     print("Support Vector Machine Classifer")
-    print(classification_report(y_test_np, y_pred_svm))
+    print(classification_report(y_test, y_pred_svm))
 
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
 model = GPT2Model.from_pretrained('gpt2')
@@ -158,7 +170,7 @@ for sentence in sentences:
         word_ids = [i for i, token in enumerate(inputs_tokens['input_ids'][0]) if tokenizer.decode([token]).strip() == word]
         for word_id in word_ids:
             # Extract and process the hidden state for each word
-            word_hidden_state = curr_hidden_states[0, word_id, :]
+            word_hidden_state = curr_hidden_states[0, word_id, :].tolist()
             hidden_states.append(word_hidden_state)
 
 print(len(hidden_states))
@@ -167,7 +179,7 @@ tags = ['comp'] * 40 + ['wh-adj'] * 80
 
 train_set, test_set, entire, that_set, whether_set, why_set, how_set, when_set, where_set = train_test_sets(hiddens=hidden_states)
 
-logistic_regression(train=train_set, test=test_set)
+# logistic_regression(train=train_set, test=test_set)
 svm(train=train_set, test=test_set)
-# incr_logistic_regression(that_set, whether_set, why_set, how_set, when_set, where_set)
-incr_rlogistic_regression(whole=entire)
+incr_logistic_regression(that_set, whether_set, why_set, how_set, when_set, where_set)
+# incr_rlogistic_regression(whole=entire)
